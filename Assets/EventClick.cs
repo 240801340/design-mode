e40427d860032778bd53644ad3436c31ca107105
codefarmer <240801340@qq.com>
2022-06-07 20:14:37 +0800

    adding the click changing color function

--- Assets/EventClick.cs
+++ Assets/EventClick.cs
@@ -3,15 +3,29 @@ using UnityEngine.EventSystems;
 
 public class EventClick : MonoBehaviour, IPointerClickHandler
 {
-
-    //定义一个颜色 来保存之前的颜色 
-    Color color;
     public void OnPointerClick(PointerEventData eventData)
     {
-        print("点击了：：" + this.name);
-        color = transform.GetComponent<MeshRenderer>().material.color;
-        transform.GetComponent<MeshRenderer>().material.color = Color.red;
+        if (NewBehaviourScript.iscolored == false)  //要是之前还没涂色的话
+        {
+            NewBehaviourScript.iscolored = true;
+            NewBehaviourScript.tf = transform;  //保存组件
+            NewBehaviourScript.color = transform.GetComponent<MeshRenderer>().material.color;//保存颜色
+            transform.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+        else     //要是之前已经涂了色了
+        {
+            
+            NewBehaviourScript.tf.GetComponent<MeshRenderer>().material.color = NewBehaviourScript.color;//还原之前的颜色
 
+            NewBehaviourScript.color = transform.GetComponent<MeshRenderer>().material.color;//保存颜色
+            
+            
+            transform.GetComponent<MeshRenderer>().material.color = Color.red;
+            NewBehaviourScript.tf = transform;  //保存组件
+        }
+        
+        
+  
     }
 
 }
--- Assets/variable.cs
+++ Assets/variable.cs
@@ -6,4 +6,8 @@ public class NewBehaviourScript : MonoBehaviour
 {
     //定义一个全局变量来看看是否被涂色
     public static bool iscolored = false;
+    //定义一个组件对象来保存
+    public static Transform tf;
+    //定义一个颜色 来保存之前的颜色(老组件) 
+    public static Color color;
 }
